8d62dddd9547081eacf413ce2310f204cd861e9b
Steven Zhang <stevezzhang@gmail.com>
2023-09-20 14:11:53 -0400

    Complete Activity 3

--- hello.py
+++ hello.py
@@ -1,13 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True  # Ensure template auto-reloading for development
+app.template_folder = 'templates'  # Set the template folder
 
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html',
+                           name=name,
+                           current_time=datetime.utcnow())
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# @app.errorhandler(500)
+# def internal_server_error(e):
+#     return render_template('500.html'), 500
+
