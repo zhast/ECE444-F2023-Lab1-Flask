24f962795cd260203857ef4bec9970237d256d5a
Steven Zhang <stevezzhang@gmail.com>
2023-09-20 16:10:15 -0400

    Complete Activity 4, step 2

--- hello.py
+++ hello.py
@@ -1,21 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-
+# Made with help from Copilot
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkeydonttellanyone'
 app.config['TEMPLATES_AUTO_RELOAD'] = True  # Ensure template auto-reloading for development
 app.template_folder = 'templates'  # Set the template folder
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EmailForm(FlaskForm):
+    email = StringField('What is your email?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
+    name_form = NameForm()
+    email_form = EmailForm()
+
+    if name_form.validate_on_submit() and email_form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != name_form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = name_form.name.data
+
+        email = email_form.email.data
+        if email.endswith('utoronto.ca'):
+            flash('Your email address is {}'.format(email))
+        else:
+            flash('Please enter a UofT email address')
+
+        return redirect(url_for('index'))  # Redirect after successful form submission
+
+    return render_template('index.html', name_form=name_form, email_form=email_form)
+
+
+
 
 
 @app.route('/user/<name>')
@@ -29,8 +63,6 @@ def user(name):
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 # @app.errorhandler(500)
 # def internal_server_error(e):
 #     return render_template('500.html'), 500
-
